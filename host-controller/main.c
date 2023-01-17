b7621276510235d783dfa01fa6ea470d63ec9b59
jthomas <julien.thomas@nagra.com>
2023-01-17 16:51:49 +0100

    Add continous read loop for debug over USB

--- host-controller/main.c
+++ host-controller/main.c
@@ -1,6 +1,8 @@
 #include <libusb-1.0/libusb.h>
 #include <ctype.h>
+#include <signal.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
@@ -19,18 +21,32 @@
 #define EP1IN  0x81
 
 
+struct libusb_device_handle *gDeviceHandle = NULL;
+
+
+void
+handler_sigint(int sig)
+{
+    libusb_release_interface(gDeviceHandle, INTERFACE);
+    libusb_close(gDeviceHandle);
+    libusb_exit(NULL);
+
+    printf("Exiting\n");
+    exit(0);
+}
 
 
 int
 main(int argc, char *argv[])
 {
-    struct libusb_device_handle *device_handle = NULL;
     int retCode;
     //char ping[] = "ping!";
     char ping[] = "debug";
-    char buffer[1024];
+    char buffer[4096];
+    const int capBuffer = 4096;
     int szBuffer = 0;
 
+    signal(SIGINT, handler_sigint);
 
     /* Prolog. */
     retCode = libusb_init(NULL);
@@ -39,21 +55,21 @@ main(int argc, char *argv[])
         return -1;
     }
 
-    device_handle = libusb_open_device_with_vid_pid(NULL, ID_VENDOR, ID_PRODUCT);
-    if (device_handle == NULL) {
+    gDeviceHandle = libusb_open_device_with_vid_pid(NULL, ID_VENDOR, ID_PRODUCT);
+    if (gDeviceHandle == NULL) {
         printf("Error finding USB device\n");
         libusb_exit(NULL);
         return -2;
     }
 
-    libusb_set_auto_detach_kernel_driver(device_handle, INTERFACE);
+    libusb_set_auto_detach_kernel_driver(gDeviceHandle, INTERFACE);
 
-    retCode = libusb_claim_interface(device_handle, 0);
+    retCode = libusb_claim_interface(gDeviceHandle, 0);
     if ( retCode < 0) {
         printf("Error claiming interface: %s\n", libusb_error_name(retCode));
-        if (device_handle)
+        if (gDeviceHandle)
         {
-            libusb_close(device_handle);
+            libusb_close(gDeviceHandle);
         }
 
         libusb_exit(NULL);
@@ -61,30 +77,34 @@ main(int argc, char *argv[])
 	}
     /* end of prolog. */
 
-    szBuffer = strnlen(ping, 1023) + 1; /* Do not forget the null terminator. */
-    memcpy(buffer, ping, szBuffer);
-    retCode = libusb_bulk_transfer(device_handle, EP1OUT, (unsigned char *)buffer, szBuffer, NULL, 0);
-    if (retCode == 0) {
-        printf("[INFO]\tData send successfully\n");
-    } else {
-        printf("[ERROR]\tData NOT send successfully\n");
-    }
+    // szBuffer = strnlen(ping, 1023) + 1; /* Do not forget the null terminator. */
+    // memcpy(buffer, ping, szBuffer);
+    // retCode = libusb_bulk_transfer(device_handle, EP1OUT, (unsigned char *)buffer, szBuffer, NULL, 0);
+    // if (retCode == 0) {
+    //     printf("[INFO]\tData send successfully\n");
+    // } else {
+    //     printf("[ERROR]\tData NOT send successfully\n");
+    // }
 
-    memset(buffer, 0x30, 1023);
+    memset(buffer, 0, capBuffer-1);
 
     // printf("Sleeping a bit before receiving data...\n");
     // usleep(100000);
     // printf("Waking up!\n");
 
-    retCode = libusb_bulk_transfer(device_handle, EP1IN, (unsigned char *)buffer, 1024, NULL, 0);
-    buffer[1023] = 0;
-    if (retCode == 0) {
-        printf("[INFO]\tData received successfully: %s\n", buffer);
-    } else {
-        printf("[ERROR]\tData NOT received successfully: %s\n", libusb_strerror(retCode));
+    while (1) {
+        retCode = libusb_bulk_transfer(gDeviceHandle, EP1IN, (unsigned char *)buffer, capBuffer, NULL, 0);
+        buffer[capBuffer-1] = 0;
+        if (retCode == 0) {
+            printf("%s", buffer);
+        } else {
+            printf("[ERROR]\tData NOT received successfully: %s\n", libusb_strerror(retCode));
+        }
+        memset(buffer, 0, capBuffer);
+        usleep(10000);
     }
 
-    for (int i = 0; i < 1024; ++i) {
+    for (int i = 0; i < capBuffer; ++i) {
         if (isprint(buffer[i])) {
             putchar(buffer[i]);
         } else {
@@ -97,8 +117,8 @@ main(int argc, char *argv[])
     }
 
     /* Epilog. */
-    libusb_release_interface(device_handle, INTERFACE);
-    libusb_close(device_handle);
+    libusb_release_interface(gDeviceHandle, INTERFACE);
+    libusb_close(gDeviceHandle);
     libusb_exit(NULL);
 
     return 0;
