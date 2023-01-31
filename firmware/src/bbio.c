25bb42e42a550e338f9fcae5fb0e852faea8c794
jthomas <julien.thomas@nagra.com>
2023-01-31 08:33:56 +0100

    Add boilerplate

--- firmware/src/bbio.c
+++ firmware/src/bbio.c
@@ -4,6 +4,23 @@
 
 #include "bbio.h"
 
+/* variables */
+
+/* _descriptorsStore is our "free store", it is a memory pool dedicated to
+ * descriptors the user will load.
+ * In the future we will have a dedicated memory allocator to manage this free
+ * store.
+ * This free store is always hidden to the user, hence the underscore as a
+ * prefix.
+ */
+static uint8_t _descriptorsStore[4096];
+
+USB_DEV_DESCR  *g_descriptorDevice;
+USB_CFG_DESCR  *g_descriptorsConfiguration[10];
+USB_ITF_DESCR  *g_descriptorsInterface[10];
+USB_ENDP_DESCR *g_descriptorsEndpoint[10];
+
+
 /* functions implementation */
 
 
--- firmware/src/bbio.h
+++ firmware/src/bbio.h
@@ -1,6 +1,11 @@
 #ifndef BBIO_H
 #define BBIO_H
 
+#include "CH56xSFR.h"
+#include "CH56x_common.h"
+
+#include "usb20.h"
+
 /* macros */
 
 
@@ -19,6 +24,14 @@ enum BbioSubCommand {
 };
 
 /* variables */
+
+/* The following global variables are pointers to structures loaded by the user.
+ *
+ */
+extern USB_DEV_DESCR  *g_descriptorDevice;
+extern USB_CFG_DESCR  *g_descriptorsConfiguration[];
+extern USB_ITF_DESCR  *g_descriptorsInterface[];
+extern USB_ENDP_DESCR *g_descriptorsEndpoint[];
 
 
 /* functions declaration */
