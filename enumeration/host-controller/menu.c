ccca37ce74682ee0c9550187569d0bd54118f294
jthomas <julien.thomas@nagra.com>
2023-02-21 08:16:48 +0100

    Update menu to take 2 digits

--- enumeration/host-controller/menu.c
+++ enumeration/host-controller/menu.c
@@ -17,14 +17,15 @@ menu_print(void)
 {
     printf("HydraDancer host controller\n");
     printf("Select your action:\n");
-    printf("1) Log once\n");
-    printf("2) Log infinite loop\n");
-    printf("3) Enumerate Audio\n");
-    printf("4) Enumerate CDC\n");
-    printf("5) Enumerate HID\n");
-    printf("6) Enumerate Image\n");
-    printf("7) Enumerate Printer\n");
-    printf("8) Enumerate Mass Storage\n");
+    printf("1)  Log once\n");
+    printf("2)  Log infinite loop\n");
+    printf("3)  Enumerate Audio\n");
+    printf("4)  Enumerate CDC\n");
+    printf("5)  Enumerate HID\n");
+    printf("6)  Enumerate Image\n");
+    printf("7)  Enumerate Printer\n");
+    printf("8)  Enumerate Mass Storage\n");
+    printf("99) Disconnect Current Device\n");
     printf("\n");
     printf("0) Exit\n");
     printf("> ");
